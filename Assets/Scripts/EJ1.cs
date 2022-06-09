df977735b5ee9508c327ddcb6f49494917671239
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-06-09 15:53:31 -0300

    finished

---
+++ Assets/Scripts/EJ1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        escribirCadena("Mi cadena es esta");
+        concatenador("Hay", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            holaMundo();
+        }
+    }
+
+    void holaMundo()
+    {
+        Debug.Log("Hola mundo");
+    }
+
+    void escribirCadena(string cadena)
+    {
+        Debug.Log(cadena);
+    }
+
+    void concatenador(string texto, int entero)
+    {
+        Debug.Log(texto + " " + entero);
+    }
+}
---
+++ Assets/Scripts/EJ2.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ2 : MonoBehaviour
+{
+    public GameObject myObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            posicionarObjetoEnOrigen(myObj);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // Debug.Log(posicionYNoNegativa(myObj));
+            if(posicionYNoNegativa(myObj) == true)
+            {
+                Debug.Log("Y es positiva");
+            }
+            else
+            {
+                Debug.Log("Y es negativa");
+            }
+        }
+    }
+
+    void posicionarObjetoEnOrigen(GameObject obj)
+    {
+        obj.transform.position = Vector3.zero; // Vector3.zero = new Vector3(0, 0, 0)
+    }
+
+    bool posicionYNoNegativa(GameObject obj)
+
+    {
+        bool YNoNegativa = false;
+        if(obj.transform.position.y >= 0)
+        {
+            YNoNegativa = true;
+        }
+        return YNoNegativa;
+    }
+}
